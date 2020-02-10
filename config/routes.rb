a334e41a10ded95c6db63fb843471722566d400f
Ambroise Drogou <ambroise.drogou@edhec.com>
2020-02-10 12:12:03 +0100

    routes, controllers, and views created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'ask', to: 'question#ask', as: :ask
+  get 'answer', to: 'question#answer', as: :answer
 end
